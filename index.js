bcd207dfb493a26e9e81371d5fcd5f31aa13faa5
Byrone Kinsly <Kinslybyrone17@gmail.com>
2022-10-21 14:45:16 +0300

    digital clock

---
+++ index.js
@@ -0,0 +1,35 @@
+const hour = document.getElementById("hrs");
+const minutes = document.getElementById("mins");
+const seconds = document.getElementById("secs");
+const tyme = document.getElementById("tyme");
+
+
+function getTime(){
+   let h = new Date().getHours(); 
+   let m = new Date().getMinutes(); 
+   let s = new Date().getSeconds(); 
+   let tyme = "PM";
+
+   if(h > 12){
+    
+    h = h - 12;
+    h = "0"+h;
+    tyme = "PM"
+    
+   }
+   if(m < 10){
+    m = "0"+m;
+   }
+   if(s < 10){
+    s = "0"+s;
+   }
+   hour.innerText = h;
+   minutes.innerText = m;
+   seconds.innerText = s;
+   document.getElementById("tyme").innerHTML = tyme;
+   setTimeout(()=>{
+    getTime()
+   })
+
+}
+getTime()
